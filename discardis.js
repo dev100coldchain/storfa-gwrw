a9b7e776f4f292a57d90bcffeb7a56cb08667950
David <david@100coldcain.com>
2025-07-26 17:08:58 -0400

    placed OnCartChanged inside OnPageLoaded

--- discardis.js
+++ discardis.js
@@ -3,9 +3,8 @@ Ecwid.OnAPILoaded.add(function () {
 
     Ecwid.OnPageLoaded.add(function (page) {
         console.log(JSON.stringify(page));
-    });
-
-    Ecwid.OnCartChanged.add(function (cart) {
-        console.log(JSON.stringify(cart));
+        Ecwid.OnCartChanged.add(function (cart) {
+            console.log(JSON.stringify(cart));
+        });
     });
 });
