38d4d57cb2fa89b33b0beb5d40d7e286ac26b630
David <david@100coldcain.com>
2025-07-26 17:34:52 -0400

    console.log(page)

--- discardis.js
+++ discardis.js
@@ -1,8 +1,6 @@
 Ecwid.OnAPILoaded.add(function () {
     console.log("Ecwid JS API is loaded.");
 
-    Ecwid.setPage('cart');
-
     Ecwid.OnPageLoaded.add(function (page) {
         if (page.type == "CART") {
             console.log(page);
