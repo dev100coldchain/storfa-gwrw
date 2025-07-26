419424707477186adebb652ba47a9896131e6d95
David <david@100coldcain.com>
2025-07-26 16:59:46 -0400

    OnPageLoaded

--- discardis.js
+++ discardis.js
@@ -1,12 +1,7 @@
 Ecwid.OnAPILoaded.add(function () {
     console.log("Ecwid JS API is loaded.");
 
-    Ecwid.Cart.calculateTotal(function (order) {
-        console.log("Calculated Order:");
-        console.log(JSON.stringify(order, null, 2));
-
-        if (order && order.total) {
-            console.log("Total Price: $" + order.total.toFixed(2));
-        }
+    Ecwid.OnPageLoad.add(function () {
+        console.log("Page DOM has just loaded");
     });
 });
