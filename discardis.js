29ad2e08193f7468a23a3c271c8a667d5d6fd2be
David <david@100coldcain.com>
2025-07-26 16:01:24 -0400

    use wrap Ecwid.OnAPILoaded.add(function () in discardis.js

--- discardis.js
+++ discardis.js
@@ -1,3 +1,10 @@
-Ecwid.Cart.calculateTotal(function (order) {
-    console.log(JSON.stringify(order));
+// Wait for Ecwid JS API to load
+Ecwid.OnAPILoaded.add(function () {
+  console.log("Ecwid JS API loaded.");
+
+  // Calculate cart total and log the order object
+  Ecwid.Cart.calculateTotal(function (order) {
+    console.log("Calculated Order Total:");
+    console.log(JSON.stringify(order, null, 2));
+  });
 });
