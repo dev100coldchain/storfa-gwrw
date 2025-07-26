bcc294a7467b4ae9b7b232a2368bfdac608d1f0f
David <david@100coldcain.com>
2025-07-26 17:20:04 -0400

    openPage

--- discardis.js
+++ discardis.js
@@ -1,10 +1,13 @@
 Ecwid.OnAPILoaded.add(function () {
     console.log("Ecwid JS API is loaded.");
 
+    Ecwid.openPage('cart')
+
     Ecwid.OnPageLoaded.add(function (page) {
         console.log(JSON.stringify(page));
+
         Ecwid.OnCartChanged.add(function (cart) {
-            console.log(JSON.stringify(cart));
-        });
+        console.log(JSON.stringify(cart));
+    });
     });
 });
