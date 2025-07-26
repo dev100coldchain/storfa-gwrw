2df92e387893f99dc96a759b6638c06c981a3f86
David <david@100coldcain.com>
2025-07-26 17:03:13 -0400

    JSON.stringify

--- discardis.js
+++ discardis.js
@@ -1,7 +1,7 @@
 Ecwid.OnAPILoaded.add(function () {
     console.log("Ecwid JS API is loaded.");
 
-    Ecwid.OnPageLoad.add(function () {
-        console.log("Page DOM has just loaded");
+    Ecwid.OnPageLoaded.add(function (page) {
+        console.log(JSON.stringify(page));
     });
 });
