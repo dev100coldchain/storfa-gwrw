ee30126f968227baa624c196e047189636852911
David <david@100coldcain.com>
2025-07-26 16:34:18 -0400

    test using callback

--- discardis.js
+++ discardis.js
@@ -1,6 +1,14 @@
-document.addEventListener('ecwidOnPageLoaded', function() {
-    Ecwid.Cart.calculateTotal(function(order) {
-        console.log('Order details:', JSON.stringify(order, null, 2));
-        // You can access order.total, order.items, etc.
-    });
+Ecwid.OnAPILoaded.add(function () {
+  console.log("Ecwid JS API is loaded.");
+
+  // Use a callback to get the calculated cart total
+  Ecwid.Cart.calculateTotal(function (order) {
+    console.log("Calculated Order:");
+    console.log(JSON.stringify(order, null, 2));
+
+    // Example: Display total price in console
+    if (order && order.total) {
+      console.log("Total Price: $" + order.total.toFixed(2));
+    }
+  });
 });
