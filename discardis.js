19ebdb0f0f14ed3fac01767658213af1a68966a1
David <david@100coldcain.com>
2025-07-26 16:57:07 -0400

    test using simple start

--- discardis.js
+++ discardis.js
@@ -1,15 +1,12 @@
-// If you get "Cannot read properties of undefined (reading 'add')", try using Ecwid.OnAPILoaded instead of Ecwid.OnAPILoaded.add:
-if (Ecwid.OnAPILoaded && typeof Ecwid.OnAPILoaded.add === "function") {
-  Ecwid.OnAPILoaded.add(function () {
+Ecwid.OnAPILoaded.add(function () {
     console.log("Ecwid JS API is loaded.");
 
     Ecwid.Cart.calculateTotal(function (order) {
-      console.log("Calculated Order:");
-      console.log(JSON.stringify(order, null, 2));
+        console.log("Calculated Order:");
+        console.log(JSON.stringify(order, null, 2));
 
-      if (order && order.total) {
-        console.log("Total Price: $" + order.total.toFixed(2));
-      }
+        if (order && order.total) {
+            console.log("Total Price: $" + order.total.toFixed(2));
+        }
     });
-  });
-}
+});
