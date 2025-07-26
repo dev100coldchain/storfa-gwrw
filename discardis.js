2bd14329bf246b77c46d2444d7f17d5716437e2b
David <david@100coldcain.com>
2025-07-26 16:43:36 -0400

    using Ecwid.OnAPILoaded instead

--- discardis.js
+++ discardis.js
@@ -1,14 +1,15 @@
-Ecwid.OnAPILoaded.add(function () {
-  console.log("Ecwid JS API is loaded.");
+// If you get "Cannot read properties of undefined (reading 'add')", try using Ecwid.OnAPILoaded instead of Ecwid.OnAPILoaded.add:
+if (typeof Ecwid.OnAPILoaded === "function") {
+  Ecwid.OnAPILoaded(function () {
+    console.log("Ecwid JS API is loaded.");
 
-  // Use a callback to get the calculated cart total
-  Ecwid.Cart.calculateTotal(function (order) {
-    console.log("Calculated Order:");
-    console.log(JSON.stringify(order, null, 2));
+    Ecwid.Cart.calculateTotal(function (order) {
+      console.log("Calculated Order:");
+      console.log(JSON.stringify(order, null, 2));
 
-    // Example: Display total price in console
-    if (order && order.total) {
-      console.log("Total Price: $" + order.total.toFixed(2));
-    }
+      if (order && order.total) {
+        console.log("Total Price: $" + order.total.toFixed(2));
+      }
+    });
   });
-});
+}
