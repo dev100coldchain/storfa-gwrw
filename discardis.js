f63eb50867bd5b0a08d2ac444b4471cfd8f0c3f5
David <david@100coldcain.com>
2025-08-02 11:19:42 -0400

    Ecwid.OnCartChanged.add(function (cart)

--- discardis.js
+++ discardis.js
@@ -15,15 +15,17 @@
           const cartContainer = document.getElementsByClassName('ec-cart__buy-and-save')[0];
           if (cartContainer) {
             console.log("'ec-cart__sidebar-inner' exists.");
-            Ecwid.Cart.calculateTotal(function (order) {
-              console.log(JSON.stringify(order));
-              const customElement = document.createElement('div');
-              // customElement.innerHTML = '<p>Bonne fin de journée !</p>';
-              // customElement.innerHTML = '<p><span style="background-color: #EEE8AA;">Remise 5% sur la prochaine commande avec paiement à réception de la facture</span></p>';
-              const highlightColor = '#EEE8AA'; // Pale Goldenrod
-              const cartTotal = (order.total * 0.05).toFixed(2); // Calculate 5% of the total
-              customElement.innerHTML = `<p><span style="background-color: ${highlightColor};">Remise 5% sur la prochaine commande avec paiement à réception de la facture : ${cartTotal} €</span></p>`;
-              cartContainer.appendChild(customElement);
+            Ecwid.OnCartChanged.add(function (cart) {
+              cart.calculateTotal(function (order) {
+                console.log(JSON.stringify(order));
+                const customElement = document.createElement('div');
+                // customElement.innerHTML = '<p>Bonne fin de journée !</p>';
+                // customElement.innerHTML = '<p><span style="background-color: #EEE8AA;">Remise 5% sur la prochaine commande avec paiement à réception de la facture</span></p>';
+                const highlightColor = '#EEE8AA'; // Pale Goldenrod
+                const cartTotal = (order.total * 0.05).toFixed(2); // Calculate 5% of the total
+                customElement.innerHTML = `<p><span style="background-color: ${highlightColor};">Remise 5% sur la prochaine commande avec paiement à réception de la facture : ${cartTotal} €</span></p>`;
+                cartContainer.appendChild(customElement);
+              });
             });
           }
         }
