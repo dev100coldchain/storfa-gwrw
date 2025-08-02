06c685b573a003001bf8267b2db8dbae2c8b6e80
David <david@100coldcain.com>
2025-08-02 10:49:49 -0400

    inline style for remise highlight

--- discardis.js
+++ discardis.js
@@ -17,7 +17,7 @@
             console.log("'ec-cart__sidebar-inner' exists.");
             const customElement = document.createElement('div');
             // customElement.innerHTML = '<p>Bonne fin de journée !</p>';
-            customElement.innerHTML = '<p><mark>Remise 5% sur la prochaine commande avec un paiement sur réception de la facture</mark></p>';
+            customElement.innerHTML = '<p><span style="background-color: lightgreen;">Remise 5% sur la prochaine commande avec un paiement sur réception de la facture</span></p>';
             cartContainer.appendChild(customElement);
           }
         }
