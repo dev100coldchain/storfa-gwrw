89f736012434f256ef080e38c1ca75554921db96
David <david@100coldcain.com>
2025-08-02 07:53:12 -0400

    if (Ecwid.Cart)

--- discardis.js
+++ discardis.js
@@ -1,6 +1,11 @@
 Ecwid.OnAPILoaded.add(function () {
   console.log("Ecwid JS API is loaded.");
-  Ecwid.Cart.calculateTotal(function (order) {
-    console.log('Ecwid.Cart.calculateTotal : ' + JSON.stringify(order));
-  });
+  if (Ecwid.Cart) { // Check if Ecwid.Cart is defined
+    Ecwid.Cart.calculateTotal(function (order) {
+      console.log('Ecwid.Cart.calculateTotal : ' + JSON.stringify(order));
+    });
+  } else {
+    console.warn("Ecwid.Cart is not available yet.");
+    // Optionally, you could retry after a short delay or listen for another event if available
+  }
 });
