305a0468445cdc14d7ff06efb7b5f30c755acc96
David <david@100coldcain.com>
2025-08-14 13:29:43 -0400

    updated loop to reference children of each wrapper

--- discardis.js
+++ discardis.js
@@ -59,18 +59,24 @@ function displayRemiseFivePercent() {
 }
 
 function removeTextFormatColonSpace() {
-  const elements = document.getElementsByClassName('ec-cart-option ec-cart-option--key');
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].innerHTML = '';
-  }
-
-  // const containers = document.getElementsByClassName('ec-cart-item__wrap');
-  // for (let i = 0; i < containers.length; i++) {
-  //   const container = containers[i];
-  //   if (container.classList.contains('ec-cart-option') && container.classList.contains('ec-cart-option--key')) {
-  //     container.innerHTML = '';
-  //   }
+  // const elements = document.getElementsByClassName('ec-cart-option ec-cart-option--key');
+  // for (let i = 0; i < elements.length; i++) {
+  //   elements[i].innerHTML = '';
   // }
+
+  const containers = document.getElementsByClassName('ec-cart-item__wrap');
+  for (let i = 0; i < containers.length; i++) {
+    const container = containers[i];
+    for (let j = 0; j < container.children.length; j++) {
+      const child = container.children[j];
+      if (child.classList.contains('ec-cart-option') && child.classList.contains('ec-cart-option--key')) {
+        child.innerHTML = '';
+      }
+    }
+
+    // if (container.classList.contains('ec-cart-option') && container.classList.contains('ec-cart-option--key')) {
+    //   container.innerHTML = '';
+  }
 }
 
 function waitForEcwidCartObject(retries, delay) {
