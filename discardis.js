1a09456e7261221febacc2eb946c12032fa3e01e
David <david@100coldcain.com>
2025-08-14 14:11:40 -0400

    limited extract to numbers immediately followed by x

--- discardis.js
+++ discardis.js
@@ -74,7 +74,7 @@ function removeTextFormatColonSpace() {
     console.log('formatValues[0] : ', formatValues[0]);
 
     if (formatValues[0]) {
-      const match = formatValues[0].textContent.match(/(\d+)(?=x)/);
+      const match = formatValues[0].textContent.match(/(\d+)x/);
       if (match) {
         const extractedNumber = match[1];
         console.log('Extracted number:', extractedNumber); // "12" from "Canette 12x44cl"
